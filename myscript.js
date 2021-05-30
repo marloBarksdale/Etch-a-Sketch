095aff8ab8feea589897c4c4a95ae0c5a6d6a2e8
marloBarksdale <lyndenflood@gmail.com>
2021-05-30 13:58:11 -0400

    added value slider and refresh button. Still very ugly but works

--- myscript.js
+++ myscript.js
@@ -1,54 +1,100 @@
 let container = document.querySelector("#container");
 
-let num = 100   ;
 
 
-container.style.display = "grid";
+var slider = document.getElementById("myRange");
+let num = slider.value;
+createGrid(num);
 
-container.style.gridTemplateColumns = `repeat(${num},1fr) `;
-container.style.gridTemplateRows = `repeat(${num},1fr) `;
+
+
+
+
+slider.oninput = function () {
+
+
+
+  container = document.querySelector('#container');
+  removeAllChildNodes(container);
+
+  num = slider.value;
+
+  createGrid(num);
+
+
+}
 
 
 
 function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
+  return color;
+}
 
-  
- 
- 
-createGrid(num);
+function refresh() {
+
+  let refreshed = document.querySelectorAll('.colored');
+
+  refreshed.forEach(function (e) {
+    e.style.backgroundColor = 'white';
+    e.classList.remove('colored');
+
+  });
+
+
+
+  //createGrid(num);
+}
+
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
 
 function createGrid(size) {
 
-    for (let i = 0; i < (size * size); i++) {
 
-        let pixel = document.createElement("div");
-        pixel.classList.add("point");
-        pixel.style.cssText = "border: 0.5px black solid";
-        container.appendChild(pixel);
+  container.style.display = "grid";
 
-        
-        pixel.addEventListener('mouseover', function(){
-
-          //console.log("you clicked " + i);
-
-          pixel.style.backgroundColor = getRandomColor();
-
-        });
-
-     
+  container.style.gridTemplateColumns = `repeat(${num},1fr) `;
+  container.style.gridTemplateRows = `repeat(${num},1fr) `;
 
 
-    }
- 
+  for (let i = 0; i < (size * size); i++) {
+
+
+
+
+    let pixel = document.createElement("div");
+    pixel.classList.add("point");
+    pixel.style.cssText = "border: 0.5px black solid";
+    container.appendChild(pixel);
+
+    pixel.style.backgroundColor = 'white';
+
+    pixel.addEventListener('mouseover', function () {
+
+      //console.log("you clicked " + i);
+      pixel.classList.add('colored');
+
+      pixel.style.backgroundColor = getRandomColor();
+
+    });
+
+
+
+
+  }
+
 }
-  
+
 
 
 // for (let i = 0; i < imageArray.length; i++) {
@@ -57,9 +103,9 @@ function createGrid(size) {
 //   });
 // }
 
-   
 
-   
+
+
 
 
 
